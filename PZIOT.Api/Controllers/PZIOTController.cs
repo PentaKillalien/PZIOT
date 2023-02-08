1f03a4bd5a25999d4f1b0211a8eb6903dc668581
1173987530 <1173987530@qq.com>
2023-02-08 18:00:30 +0800

    --

--- PZIOT.Api/Controllers/PZIOTController.cs
+++ PZIOT.Api/Controllers/PZIOTController.cs
@@ -11,9 +11,10 @@ namespace PZIOT.Api.Controllers
     /// IOT相关服务启动
     /// </summary>
     [Produces("application/json")]
-    [Route("api/Pziot")]
+    [Route("api/[controller]/[action]")]
     public class PZIOTController : BaseApiController
     {
+        //IOT只需要一个入口启动就行了，不需要太多的内容，如果需要把报表什么的数据结构暴露出来，应该建立其它的服务进行操作
         readonly IPZIOTServices _PZIOTServices;
         /// <summary>
         /// 构造函数注入
