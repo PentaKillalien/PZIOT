cbb38d2693e34c44fe917a3063a95ca17cd08e8c
1173987530 <1173987530@qq.com>
2023-02-10 10:27:37 +0800

    --设备模型抽象

--- PZIOT.Model/Models/Equipment.cs
+++ PZIOT.Model/Models/Equipment.cs
@@ -4,16 +4,15 @@ using System;
 namespace PZIOT.Model.Models
 {
     /// <summary>
-    /// 博客文章
+    /// 设备基础信息
     /// </summary>
     public class Equipment: RootEntityTkey<int>
     {
         /// <summary>
-        /// FactoryId
+        /// 设备名
         /// </summary>
         [SugarColumn(Length = 100, IsNullable = true)]
         public string Name { get; set; }
-
         /// <summary>
         /// 唯一编号
         /// </summary>
@@ -24,6 +23,34 @@ namespace PZIOT.Model.Models
         /// </summary>
         [SugarColumn(Length = 100, IsNullable = true)]
         public string Type { get; set; }
-
+        /// <summary>
+        /// 车间
+        /// </summary>
+        [SugarColumn(Length = 100, IsNullable = true)]
+        public string WorkSpace { get; set; }
+        /// <summary>
+        /// 工厂
+        /// </summary>
+        [SugarColumn(Length = 100, IsNullable = true)]
+        public string Factory { get; set; }
+        /// <summary>
+        /// 位置
+        /// </summary>
+        [SugarColumn(Length = 100, IsNullable = true)]
+        public string Position { get; set; }
+        /// <summary>
+        /// 产线
+        /// </summary>
+        [SugarColumn(Length = 100, IsNullable = true)]
+        public string ProductLine { get; set; }
+        /// <summary>
+        /// 注册用户 --哪个用户注册的
+        /// </summary>
+        [SugarColumn(Length = 100, IsNullable = true)]
+        public string RegisterUser { get; set; }
+        /// <summary>
+        /// 注册用Id --唯一，作为键
+        /// </summary>
+        public int RegisterUserId { get; set; }
     }
 }
