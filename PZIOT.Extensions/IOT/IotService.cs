d3dcf6bdbcbf53e8e8e5204c310e875b700e2d3c
1173987530 <1173987530@qq.com>
2023-02-15 18:30:14 +0800

    即将进入Mates项配置

--- PZIOT.Extensions/IOT/IotService.cs
+++ PZIOT.Extensions/IOT/IotService.cs
@@ -11,6 +11,10 @@ using System.Threading.Tasks;
 
 namespace PZIOT.Extensions.IOT
 {
+    /// <summary>
+    /// 此服务支持的是私有部署，不上云，走现场网络的改造，上云走mqtt,设置权限等，Iot不启动，后面加入配置
+    /// 上云用不到驱动描述表，这是前提
+    /// </summary>
     public class IotService : IHostedService, IDisposable
     {
         private IEquipmentDriverDescServices _euqipmentDriverDescServices;
@@ -26,14 +30,17 @@ namespace PZIOT.Extensions.IOT
         }
         public async Task StartAsync(CancellationToken cancellationToken)
         {
+            //需要定义个容器挂载所有的驱动连接，配置数据项后统一调用接口读取相关数据进行采集活动
             ConsoleHelper.WriteSuccessLine("StartedAsync: IOT服务已启动.");
             Log.Info("StartedAsync: IOT服务已启动.");
+            //开始加载设备驱动,从数据库读取，读取时根据设备编号建立映射
             List<EquipmentDriverDesc> tempAllDrivers = await _euqipmentDriverDescServices.Query();
             if (tempAllDrivers.Count == 0)
                 ConsoleHelper.WriteWarningLine("数据库无任何驱动配置信息");
             else
                 await new EquipmentDriverDescOper().CreatAllDriverConnection(tempAllDrivers);
-            //开始加载设备驱动,从数据库读取，读取时根据设备编号建立映射
+            //开始加载设备数据项读取相关的内容，设备数据项的表怎么建立，然后建立定时任务，读取设备信息
+            
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
