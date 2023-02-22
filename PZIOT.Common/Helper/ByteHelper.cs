605589254427902aa5c929554a7537016eae5207
ppp <2908059362@qq.com>
2023-02-22 16:39:58 +0800

    --

---
+++ PZIOT.Common/Helper/ByteHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZIOT.Common.Helper
+{
+    public class ByteHelper
+    {
+        /// <summary>
+        /// 去除byte[]数组缓冲区内的尾部空白区;从末尾向前判断;
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static byte[] BytesTrimEnd(byte[] bytes)
+        {
+            List<byte> list = bytes.ToList();
+            for (int i = bytes.Length - 1; i >= 0; i--)
+            {
+                if (bytes[i] == 0x00)
+                {
+                    list.RemoveAt(i);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return list.ToArray();
+        }
+    }
+}
